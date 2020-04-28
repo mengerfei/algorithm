a697deb2ac1eb4f4e268a5a7c05e7ec5bfd256f1
mengerfei <txgzlove@gmail.com>
2020-04-28 16:02:37 +0800

    选择排序

---
+++ 选择算法.js
@@ -0,0 +1,21 @@
+// minIndex重写
+let minIndex = (numbers) => {
+  let index = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < numbers[index]) {
+      index = i;
+    }
+  }
+  return index;
+};
+
+// 所有的递归都可以改成循环
+
+// 调换位置
+let swap = (array, i, j) => {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+};
+
+// 调用：swap(numbers,1,2)
